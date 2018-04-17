1f18a29c6d8e9691e2821ab39467cd6b2d07368a
yoghesh <yoghesh.d@gmail.com>
2018-04-18 02:07:03 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+app.get('/', function(req,res){
+	res.send('Hello world');
+});
+
+
+server.listen(server_port, server_ip, function(){
+	console.log("Application started")
+});
