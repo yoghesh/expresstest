3547abf1848e00c6540ee0f30ae5507a4bdbe29a
yoghesh <yoghesh.d@gmail.com>
2018-04-18 02:55:43 +0530

    four commit

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 var express = require('express');
 var app = express();
 var tunnel = require('node-local-tunnel');
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
-var server_ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-var options = {
-    localBase: 'http://localhost:3000/*'
-};
-tunnel.init();
-tunnel.client(options);
-app.use(tunnel.server());
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+// var options = {
+    // localBase: 'http://0.0.0.0:3000/*'
+// };
+// tunnel.init();
+// tunnel.client(options);
+// app.use(tunnel.server());
 app.get('/', function(req,res){
 	res.send('Hello world');
 });
 
 
-app.listen(3000, server_ip, function(){
+app.listen(server_port, server_ip, function(){
 	console.log("Application started in " + server_ip + " "  + server_port);
 });
