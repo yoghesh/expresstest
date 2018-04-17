0ab826fd2dbbff2c23ee70618821f486355f82ab
yoghesh <yoghesh.d@gmail.com>
2018-04-18 02:35:31 +0530

    third commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_ip = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
 app.get('/', function(req,res){
 	res.send('Hello world');
