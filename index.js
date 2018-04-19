6e97c7678ac1556f72a4880b6abc6abb24064d4a
yoghesh <yoghesh.d@gmail.com>
2018-04-20 01:26:17 +0530

    fifth commit

--- index.js
+++ index.js
@@ -2,13 +2,7 @@ var express = require('express');
 var app = express();
 var tunnel = require('node-local-tunnel');
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
-// var options = {
-    // localBase: 'http://0.0.0.0:3000/*'
-// };
-// tunnel.init();
-// tunnel.client(options);
-// app.use(tunnel.server());
+var server_ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 app.get('*', function(req,res){
 	res.send('Hello world');
 });
