e214de66e545eadcb3e7f0af09ddd6718e7363f8
yoghesh <yoghesh.d@gmail.com>
2018-04-20 03:11:03 +0530

    eight commit

--- index.js
+++ index.js
@@ -1,22 +1,88 @@
- var express = require('express');
- var app = express();
-// var tunnel = require('node-local-tunnel');
-// var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-// var server_ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
- app.get('*', function(req,res){
-	 res.send('Hello world');
+ // var express = require('express');
+ // var app = express();
+// // var tunnel = require('node-local-tunnel');
+// // var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+// // var server_ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ // app.get('*', function(req,res){
+	 // res.send('Hello world');
+ // });
+// app.listen(8080);
+// console.log("Application started");
+
+// // app.listen(server_port, server_ip, function(){
+	// // console.log("Application started in " + server_ip + " "  + server_port);
+// // });
+
+ var http = require('http');
+var url = require('url');
+ var server = http.createServer(function(req, res) {
+	 var q = url.parse(req.url, true);
+	 console.log(q.pathname);
+   res.writeHead(200);
+   if(req.url === '/boundary'){
+   http.get('http://www.cricbuzz.com/cricket-series/api/stats/highlights-list?statsType=boundary&seriesType=IPL&seriesId=2676',
+   (resp)=> {
+	   resp.on('data',(chunk)=>{
+		   res.write(chunk);
+	   });
+	   resp.on('end', ()=>{
+		   	   res.end();
+	   });
+   });
+   }
+   else if(req.url === '/wickets'){
+   http.get('http://www.cricbuzz.com/cricket-series/api/stats/zone?statsType=mostWickets&seriesType=IPL&seriesId=2676',
+   (resp)=> {
+	   resp.on('data',(chunk)=>{
+		   res.write(chunk);
+	   });
+	   resp.on('end', ()=>{
+		   	   res.end();
+	   });
+   });
+   }
+   else if(req.url === '/runs'){
+   http.get('http://www.cricbuzz.com/cricket-series/api/stats/zone?statsType=mostRuns&seriesType=IPL&seriesId=2676',
+   (resp)=> {
+	   resp.on('data',(chunk)=>{
+		   res.write(chunk);
+	   });
+	   resp.on('end', ()=>{
+		   	   res.end();
+	   });
+   });
+   }
+   else if( q.pathname === '/match')
+   {
+	   var id = url.parse(req.url, true).query;
+	   http.get('http://www.cricbuzz.com/match-api/'+id.id+'/commentary.json',
+   (resp)=> {
+	   resp.on('data',(chunk)=>{
+		   res.write(chunk);
+	   });
+	   resp.on('end', ()=>{
+		   	   res.end();
+	   });
+   });
+   }
+   else if( q.pathname === '/matchfull')
+   {
+	   var id = url.parse(req.url, true).query;
+	   http.get('http://www.cricbuzz.com/match-api/'+id.id+'/commentary-full.json',
+   (resp)=> {
+	   resp.on('data',(chunk)=>{
+		   res.write(chunk);
+	   });
+	   resp.on('end', ()=>{
+		   	   res.end();
+	   });
+   });
+   }
+   else{
+	   res.write('Hello');
+	   res.end();
+   }
+   
+   //res.end('Hello World');
  });
-app.listen(8080);
-console.log("Application started");
-
-// app.listen(server_port, server_ip, function(){
-	// console.log("Application started in " + server_ip + " "  + server_port);
-// });
-
-// var http = require('http');
-
-// var server = http.createServer(function(req, res) {
-  // res.writeHead(200);
-  // res.end('Hello World');
-// });
-// server.listen(8080);
+ server.listen(8080);
