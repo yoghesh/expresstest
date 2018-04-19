c5bc0eefaa3548832f3f838f475591c9cbc03f42
yoghesh <yoghesh.d@gmail.com>
2018-04-20 01:35:49 +0530

    sixth commit

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-var express = require('express');
-var app = express();
-var tunnel = require('node-local-tunnel');
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-app.get('*', function(req,res){
-	res.send('Hello world');
-});
+// var express = require('express');
+// var app = express();
+// var tunnel = require('node-local-tunnel');
+// var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+// var server_ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+// app.get('*', function(req,res){
+	// res.send('Hello world');
+// });
 
 
-app.listen(server_port, server_ip, function(){
-	console.log("Application started in " + server_ip + " "  + server_port);
+// app.listen(server_port, server_ip, function(){
+	// console.log("Application started in " + server_ip + " "  + server_port);
+// });
+
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end('Hello World');
 });
+server.listen(8080);
